966c036a034d9bb1e0fb65ba1cb8838b2e0b504d
Bullrunonsol <BullRunonsol@protonmail.com>
2024-11-19 05:59:46 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    var options = {
+        chart: {
+            type: 'donut'
+        },
+        series: [5, 5, 5, 85],
+        labels: ['Marketing', 'Reservas', 'Crecimiento Futuro', 'Liquidez']
+    };
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+});
